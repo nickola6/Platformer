69a052d51c3680f083f122be89d72c5e9b8597b2
nick <kolya.2003.yskov@mail.ru>
2026-06-02 11:35:39 +0300

    Adding a directional change event

--- Scripts/InputReader.cs
+++ Scripts/InputReader.cs
@@ -5,12 +5,14 @@ public class InputReader : MonoBehaviour
     private const string Horizontal = "Horizontal";
     private const KeyCode JumpKey = KeyCode.Space;
 
+    public event Action<float> DirectionChanged;
     public float Direction { get; private set; }
     public bool IsJump { get; private set; }
 
     private void Update()
     {
         Direction = Input.GetAxisRaw(Horizontal);
+        DirectionChanged?.Invoke(Direction);
         IsJump = Input.GetKeyDown(JumpKey);
     }
 
