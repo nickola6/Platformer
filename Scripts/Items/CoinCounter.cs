c608c827d0cea8aa6190ebbfb7a1dad966eadd59
nick <kolya.2003.yskov@mail.ru>
2026-06-06 22:44:37 +0300

    Deleting FindObjectsOfType

--- Scripts/Items/CoinCounter.cs
+++ Scripts/Items/CoinCounter.cs
@@ -5,29 +5,11 @@ public class CoinCounter : MonoBehaviour
 {
     public event Action CountHasChanged;
 
-    private Coin[] _coins;
     private int _countCoins;
 
     public int CountCoins => _countCoins;
 
-    private void Start()
-    {
-        _coins = FindObjectsOfType<Coin>();
-
-        foreach (Coin coin in _coins)
-            coin.PickedUp += HandleCoinPickedUp;
-    }
-
-    private void OnDestroy()
-    {
-        foreach (Coin coin in _coins)
-        {
-            if (coin != null)
-                coin.PickedUp -= HandleCoinPickedUp;
-        }
-    }
-
-    private void HandleCoinPickedUp()
+    public void AddCoin()
     {
         _countCoins++;
         CountHasChanged?.Invoke();
