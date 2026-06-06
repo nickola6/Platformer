7ab7a09ad5e9c194990a033007cf2e044f6e1f19
nick <kolya.2003.yskov@mail.ru>
2026-06-06 22:45:16 +0300

    Deleting responsibility

--- Scripts/Items/Coin.cs
+++ Scripts/Items/Coin.cs
@@ -1,12 +1,4 @@
 using System;
 using UnityEngine;
 
-public class Coin : MonoBehaviour
-{
-    public event Action PickedUp;
-
-    public void Collect()
-    {
-        PickedUp?.Invoke();
-    }
-}
+public class Coin : MonoBehaviour { }
